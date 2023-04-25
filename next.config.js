b9edcd5b48614b77e288fdd612765b14ec7552d1
Sukanta Das <arnobh84@gmail.com>
2023-04-25 14:07:31 +0600

    changes in setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    images: {
+        domains: ['localhost', 'i.ibb.co'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**'
+            }
+        ]
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')]
+    },
+    env: {
+        BACKEND_BASE_URL: process.env.BACKEND_BASE_URL
+    }
 };
 
 module.exports = nextConfig;

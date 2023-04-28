52f05f120b2a7e7e653c7899a0bd6a22801e7107
Sukanta Das <arnobh84@gmail.com>
2023-04-28 12:45:48 +0600

    antd  removed

--- libs/Components/Header/Header.tsx
+++ libs/Components/Header/Header.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import { useAuth } from '@/libs/Context/AuthProvider';
+import React, { useState } from 'react';
 
 const Header = () => {
+    const { currentUser } = useAuth();
+    const [showDropDown, setShowDropDown] = useState(false);
+
+    const handleShowDropDown = () => {
+        setShowDropDown(!showDropDown);
+    };
+    console.log(showDropDown);
     return (
         <div>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -20,70 +28,71 @@ const Header = () => {
                             type="button"
                             className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                             id="user-menu-button"
-                            aria-expanded="false"
-                            data-dropdown-toggle="user-dropdown"
-                            data-dropdown-placement="bottom"
+                            onClick={handleShowDropDown}
                         >
                             <span className="sr-only">Open user menu</span>
                             <img
                                 className="w-8 h-8 rounded-full"
-                                src="/docs/images/people/profile-picture-3.jpg"
+                                src={currentUser?.profilePicture}
                                 alt="user photo"
                             />
                         </button>
                         {/* <!-- Dropdown menu --> */}
-                        <div
-                            className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                            id="user-dropdown"
-                        >
-                            <div className="px-4 py-3">
-                                <span className="block text-sm text-gray-900 dark:text-white">
-                                    Bonnie Green
-                                </span>
-                                <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                                    name@flowbite.com
-                                </span>
+                        {showDropDown && (
+                            <div
+                                className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                                id="user-dropdown"
+                            >
+                                <div className="px-4 py-3">
+                                    <span className="block text-sm text-gray-900 dark:text-white">
+                                        Bonnie Green
+                                    </span>
+                                    <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                                        name@flowbite.com
+                                    </span>
+                                </div>
+                                <ul className="py-2" aria-labelledby="user-menu-button">
+                                    <li>
+                                        <a
+                                            href="#"
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                        >
+                                            Dashboard
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#"
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                        >
+                                            Settings
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#"
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                        >
+                                            Earnings
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#"
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                        >
+                                            Sign out
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
-                            <ul className="py-2" aria-labelledby="user-menu-button">
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                    >
-                                        Dashboard
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                    >
-                                        Settings
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                    >
-                                        Earnings
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                    >
-                                        Sign out
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                        )}
                         <button
                             data-collapse-toggle="mobile-menu-2"
                             type="button"
                             className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                             aria-controls="mobile-menu-2"
+                            onClick={handleShowDropDown}
                             aria-expanded="false"
                         >
                             <span className="sr-only">Open main menu</span>

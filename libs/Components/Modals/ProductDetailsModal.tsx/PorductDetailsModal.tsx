54ff97bbaa17582271c1c610b0b6bb7952cb1aac
Sukanta Das <arnobh84@gmail.com>
2023-04-30 11:19:57 +0600

    more  change in modal

--- libs/Components/Modals/ProductDetailsModal.tsx/PorductDetailsModal.tsx
+++ libs/Components/Modals/ProductDetailsModal.tsx/PorductDetailsModal.tsx
@@ -10,6 +10,7 @@ import {
 } from 'react-icons/ai';
 import { RxCross1 } from 'react-icons/rx';
 const ProductDetailsModal = ({ data, setShowProductDetails }: any) => {
+    console.log(data);
     const [count, setCount] = useState(1);
     const [click, setClick] = useState(false);
 
@@ -27,139 +28,112 @@ const ProductDetailsModal = ({ data, setShowProductDetails }: any) => {
         <div>
             <input type="checkbox" id="ProductDetails" className="modal-toggle" />
             <div className="modal">
-                <div className="modal-box w-11/12 max-w-5xl">
+                <div className="modal-box w-11/12 max-w-4xl">
                     {/* <--------------------------------------------> */}
-                    <div className="bg-[#fff]">
-                        {data ? (
-                            <div className="fixed w-full h-screen top-0 left-0 bg-[#00000030] z-40 flex items-center justify-center">
-                                <div className="w-[90%] 800px:w-[60%] h-[90vh] overflow-y-scroll 800px:h-[75vh] bg-white rounded-md shadow-sm relative p-4">
-                                    <RxCross1
-                                        size={30}
-                                        className="absolute right-3 top-3 z-50"
-                                        onClick={() => setShowProductDetails(false)}
-                                    />
+                    <>
+                        <div className="flex flex-wrap">
+                            <div>
+                                <Image
+                                    height={400}
+                                    width={400}
+                                    src={data?.image_Url[0]?.url}
+                                    alt="productImage"
+                                    className="h-auto  rounded-lg"
+                                />
 
-                                    <div className="block w-full 800px:flex">
-                                        <div className="w-full 800px:w-[50%]">
+                                <div>
+                                    <div className="flex ml-5 -mt-4">
+                                        <Link
+                                            href={`/shop/preview/${data.shop._id}`}
+                                            className="flex"
+                                        >
                                             <Image
-                                                height={400}
-                                                width={400}
-                                                src={data?.image_Url[0]?.url}
+                                                src={data?.shop?.shop_avatar?.url}
                                                 alt=""
+                                                height={300}
+                                                width={300}
+                                                className="w-[50px] h-[50px] rounded-full mr-2"
                                             />
-                                            <div className="flex">
-                                                <Link
-                                                    href={`/shop/preview/${data.shop._id}`}
-                                                    className="flex"
-                                                >
-                                                    <Image
-                                                        src={`${data?.shop?.shop_avatar}`}
-                                                        alt=""
-                                                        height={300}
-                                                        width={300}
-                                                        className="w-[50px] h-[50px] rounded-full mr-2"
-                                                    />
-                                                    <div>
-                                                        <h3 className={`${styles.shop_name}`}>
-                                                            {data.shop.name}
-                                                        </h3>
-                                                        <h5 className="pb-3 text-[15px]">
-                                                            (4.5) Ratings
-                                                        </h5>
-                                                    </div>
-                                                </Link>
-                                            </div>
-                                            <div
-                                                className={`${styles.button} bg-[#000] mt-4 rounded-[4px] h-11`}
-                                                // onClick={handleMessageSubmit}
-                                            >
-                                                <span className="text-[#fff] flex items-center">
-                                                    Send Message{' '}
-                                                    <AiOutlineMessage className="ml-1" />
-                                                </span>
-                                            </div>
-                                            <h5 className="text-[16px] text-[red] mt-5">
-                                                (50) Sold out
-                                            </h5>
-                                        </div>
-
-                                        <div className="w-full 800px:w-[50%] pt-5 pl-[5px] pr-[5px]">
-                                            <h1 className={`${styles.productTitle} text-[20px]`}>
-                                                {data.name}
-                                            </h1>
-                                            <p>{data.description}</p>
-
-                                            <div className="flex pt-3">
-                                                <h4 className={`${styles.productDiscountPrice}`}>
-                                                    {data.discountPrice}$
-                                                </h4>
-                                                <h3 className={`${styles.price}`}>
-                                                    {data.originalPrice
-                                                        ? data.originalPrice + '$'
-                                                        : null}
+                                            <div>
+                                                <h3 className={`${styles.shop_name}`}>
+                                                    {data.shop.name}
                                                 </h3>
+                                                <h5 className="pb-3 text-[15px]">(4.5) Ratings</h5>
                                             </div>
-                                            <div className="flex items-center mt-12 justify-between pr-3">
-                                                <div>
-                                                    <button
-                                                        className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
-                                                        onClick={decrementCount}
-                                                    >
-                                                        -
-                                                    </button>
-                                                    <span className="bg-gray-200 text-gray-800 font-medium px-4 py-[11px]">
-                                                        {count}
+                                        </Link>
+                                    </div>
+
+                                    <div
+                                        className={` mt-4 rounded-[4px] h-11 flex justify-center border cursor-pointer border-gray-300 `}
+                                        // onClick={handleMessageSubmit}
+                                    >
+                                        <span className="text-black flex items-center">
+                                            Send Message <AiOutlineMessage className="ml-1" />
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                                    {data?.name}
+                                </h1>
+                                <p className="text-justify text-sm font-medium mt-3">
+                                    {data?.description}
+                                </p>
+                                <div className="mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                                    <div className="flex justify-between">
+                                        <div className="flex">
+                                            <h4 className={`${styles.productDiscountPrice}`}>
+                                                {data?.discount_price}$
+                                            </h4>
+                                            <h3 className={`${styles.price}`}>
+                                                {data?.price ? data.price + '$' : null}
+                                            </h3>
+                                        </div>
+                                        <div>
+                                            <div className="flex">
+                                                <button
+                                                    data-action="decrement"
+                                                    className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
+                                                >
+                                                    <span className="m-auto text-2xl font-thin">
+                                                        −
                                                     </span>
-                                                    <button
-                                                        className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
-                                                        onClick={incrementCount}
-                                                    >
+                                                </button>
+                                                <input
+                                                    type="number"
+                                                    className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
+                                                    name="custom-input-number"
+                                                    value="0"
+                                                ></input>
+                                                <button
+                                                    data-action="increment"
+                                                    className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
+                                                >
+                                                    <span className="m-auto text-2xl font-thin">
                                                         +
-                                                    </button>
-                                                </div>
-                                                <div>
-                                                    {click ? (
-                                                        <AiFillHeart
-                                                            size={30}
-                                                            className="cursor-pointer"
-                                                            // onClick={() =>
-                                                            //     removeFromWishlistHandler(data)
-                                                            // }
-                                                            color={click ? 'red' : '#333'}
-                                                            title="Remove from wishlist"
-                                                        />
-                                                    ) : (
-                                                        <AiOutlineHeart
-                                                            size={30}
-                                                            className="cursor-pointer"
-                                                            // onClick={() =>
-                                                            //     addToWishlistHandler(data)
-                                                            // }
-                                                            title="Add to wishlist"
-                                                        />
-                                                    )}
-                                                </div>
-                                            </div>
-                                            <div
-                                                className={`${styles.button} mt-6 rounded-[4px] h-11 flex items-center`}
-                                                // onClick={() => addToCartHandler(data._id)}
-                                            >
-                                                <span className="text-[#fff] flex items-center">
-                                                    Add to cart{' '}
-                                                    <AiOutlineShoppingCart className="ml-1" />
-                                                </span>
+                                                    </span>
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                                <div className="flex justify-center">
+                                    <button className="flex  text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+                                        <span className="flex items-center">
+                                            Add to cart <AiOutlineShoppingCart className="ml-1" />
+                                        </span>
+                                    </button>
+                                </div>
                             </div>
-                        ) : null}
-                    </div>
-                    <div className="modal-action">
-                        <label htmlFor="ProductDetails" className="btn">
-                            Yay!
-                        </label>
-                    </div>
+                        </div>
+                        <RxCross1
+                            size={20}
+                            className="absolute right-2 top-3 z-50"
+                            onClick={() => setShowProductDetails(false)}
+                        />
+                    </>
                 </div>
             </div>
         </div>

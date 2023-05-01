1a46f4594c910e754b15e6d22e5c64fbf322ada4
Sukanta Das <arnobh84@gmail.com>
2023-05-01 15:40:43 +0600

    shange in product page

--- libs/modules/product/product.tsx
+++ libs/modules/product/product.tsx
@@ -11,21 +11,22 @@ const Product = () => {
     const categoryData = query?.category;
 
     useEffect(() => {
-        if (categoryData !== null) {
+        if (categoryData === null) {
             const d = productData && productData.sort((a, b) => b.total_sell - a.total_sell);
             setData(d);
         } else {
-            const d = productData;
+            const d = productData && productData.filter((i) => i.category === categoryData);
             setData(d);
         }
         //    window.scrollTo(0,0);
     }, [productData, categoryData]);
+    console.log(data);
 
     return (
         <div>
             <HeaderAndFooter>
                 <div>
-                    <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px]  xl:gap-[30px] mb-12">
+                    <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px]  xl:gap-[30px] mb-12">
                         {data &&
                             data.map((i: any, index: any) => <ProductCard data={i} key={index} />)}
                     </div>

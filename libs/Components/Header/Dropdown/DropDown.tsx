06c57e19bc508c9783b8ed4d4fe0d3fafc7dc75b
Sukanta Das <arnobh84@gmail.com>
2023-05-01 21:06:04 +0600

    dropdown fixed

--- libs/Components/Header/Dropdown/DropDown.tsx
+++ libs/Components/Header/Dropdown/DropDown.tsx
@@ -1,7 +1,39 @@
+import styles from '@/styles/styles';
+import { useRouter } from 'next/router';
 import React from 'react';
 
-const DropDown = ({}: any) => {
-    return <div></div>;
+const DropDown = ({ setDropDown, categoriesData }: any) => {
+    const navigate = useRouter();
+    const submitHandle = (i: any) => {
+        navigate.push(`/products?category=${i.title}`);
+        setDropDown(false);
+        window.location.reload();
+    };
+    return (
+        <div className="pb-4 w-68 bg-[#fff] absolute z-30 rounded-b-md shadow-sm">
+            {categoriesData &&
+                categoriesData.map((i: any, index: any) => (
+                    <div
+                        key={index}
+                        className={`${styles.noramlFlex}`}
+                        onClick={() => submitHandle(i)}
+                    >
+                        <img
+                            src={i.image_Url}
+                            style={{
+                                width: '25px',
+                                height: '25px',
+                                objectFit: 'contain',
+                                marginLeft: '10px',
+                                userSelect: 'none'
+                            }}
+                            alt=""
+                        />
+                        <h3 className="m-3 cursor-pointer select-none">{i.title}</h3>
+                    </div>
+                ))}
+        </div>
+    );
 };
 
 export default DropDown;
--- libs/Components/Header/SimpleNav.tsx
+++ libs/Components/Header/SimpleNav.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { AiOutlineAppstore, AiFillCaretDown } from 'react-icons/ai';
-import Link from 'next/link';
-import { categoriesData } from '@/libs/common/constant/Data';
 import { AiOutlineHeart, AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
 import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io';
 import { BiMenuAltLeft } from 'react-icons/bi';
+import Link from 'next/link';
+import { categoriesData } from '@/libs/common/constant/Data';
 import DropDown from './Dropdown/DropDown';
 
 const SimpleNav = () => {
@@ -17,55 +16,27 @@ const SimpleNav = () => {
                         <ul className="items-stretch hidden space-x-3 md:flex justify-center ">
                             <li className="flex">
                                 <div className="flex border-r-2 pr-2">
-                                    <div
-                                        className="flex w-60  items-center px-4 py-2 -mb-1 border-r-2 mr-2 border  justify-between  border-gray-600 mt-1 dropdown cursor-pointer"
-                                        tabIndex={0}
-                                    >
-                                        {/* <div className="flex items-center">
-                                            <span className="text-white mr-1">
-                                                {' '}
-                                                <AiOutlineAppstore size={25} className="" />
-                                            </span>{' '}
-                                            Shop Category
-                                        </div>
-                                        <div className="text-white ">
-                                            <AiFillCaretDown />
-                                        </div>
-
-                                        <ul
-                                            tabIndex={0}
-                                            className="dropdown-content menu p-2 mt-[37rem] shadow bg-base-100 -z-50 rounded-md w-64 -ml-6 text-black "
-                                        >
-                                            {categoriesData?.map((e: any, i: any) => {
-                                                return (
-                                                    <Link
-                                                        href={`/product?category=${e?.title}`}
-                                                        key={i}
-                                                        tabIndex={0}
-                                                    >
-                                                        <li className="my-2 text-lg py-1 px-1    rounded-md hover:bg-gray-100 font-medium">
-                                                            {e?.title}
-                                                        </li>
-                                                    </Link>
-                                                );
-                                            })}
-                                        </ul> */}
+                                    <div className="flex w-60  items-center px-4 py-2 -mb-1 border-r-2 mr-2 border  justify-between  border-gray-600 mt-1 dropdown cursor-pointer">
                                         <div onClick={() => setDropDown(!dropDown)}>
-                                            <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block">
+                                            <div className="relative h-[60px] mt-[10px] w-[270px]">
                                                 <BiMenuAltLeft
                                                     size={30}
                                                     className="absolute top-3 left-2"
                                                 />
-                                                <button
-                                                    className={`h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md`}
+                                                {/* <button
+                                                    className={`h-[100%] w-full flex justify-between items-center pl-10 text-white font-sans text-lg font-[500] select-none rounded-t-md`}
                                                 >
+                                                    
+                                                </button> */}
+                                                <span className="h-[90%] flex justify-between items-center ml-10">
                                                     All Categories
-                                                </button>
-                                                <IoIosArrowDown
-                                                    size={20}
-                                                    className="absolute right-2 top-4 cursor-pointer"
-                                                    onClick={() => setDropDown(!dropDown)}
-                                                />
+                                                    <IoIosArrowDown
+                                                        size={20}
+                                                        className="absolute right-14 top-4 cursor-pointer"
+                                                        onClick={() => setDropDown(!dropDown)}
+                                                    />
+                                                </span>
+
                                                 {dropDown ? (
                                                     <DropDown
                                                         categoriesData={categoriesData}

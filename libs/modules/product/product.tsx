b8ab90f82e2ed15621bcd4f7dd57be5b54cd6c16
Sukanta Das <arnobh84@gmail.com>
2023-05-01 15:07:58 +0600

    product page completed

--- libs/modules/product/product.tsx
+++ libs/modules/product/product.tsx
@@ -1,7 +1,6 @@
 import ProductCard from '@/libs/Components/ProductCard/ProductCard';
 import HeaderAndFooter from '@/libs/Layout/HeaderAndFooter/headerAndFooter';
 import { productData } from '@/libs/common/constant/Data';
-import styles from '@/styles/styles';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
@@ -12,11 +11,11 @@ const Product = () => {
     const categoryData = query?.category;
 
     useEffect(() => {
-        if (categoryData === null) {
-            const d = productData;
+        if (categoryData !== null) {
+            const d = productData && productData.sort((a, b) => b.total_sell - a.total_sell);
             setData(d);
         } else {
-            const d = productData && productData.filter((i) => i.category === categoryData);
+            const d = productData;
             setData(d);
         }
         //    window.scrollTo(0,0);
@@ -26,12 +25,12 @@ const Product = () => {
         <div>
             <HeaderAndFooter>
                 <div>
-                    <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px]  xl:gap-[30px] mb-12">
+                    <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px]  xl:gap-[30px] mb-12">
                         {data &&
                             data.map((i: any, index: any) => <ProductCard data={i} key={index} />)}
                     </div>
                     {data && data.length === 0 ? (
-                        <h1 className="text-center w-full pb-[100px] text-[20px]">
+                        <h1 className="text-center w-full font-mono font-bold text-5xl mb-40">
                             No products Found!
                         </h1>
                     ) : null}

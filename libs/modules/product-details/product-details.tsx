28fa9867b9b188ea37f095b1847fcdf1011b28db
Sukanta Das <arnobh84@gmail.com>
2023-05-02 20:02:24 +0600

    product details page completed

--- libs/modules/product-details/product-details.tsx
+++ libs/modules/product-details/product-details.tsx
@@ -1,10 +1,13 @@
+import ProductDetail from '@/libs/Components/ProductDetail/ProductDetail';
+import HeaderAndFooter from '@/libs/Layout/HeaderAndFooter/headerAndFooter';
 import React from 'react';
 
 const ProductDetails = ({ data }: any) => {
-    console.log(data);
     return (
         <div>
-            <p>{data?.name}</p>
+            <HeaderAndFooter>
+                <ProductDetail data={data} />
+            </HeaderAndFooter>
         </div>
     );
 };

0d40334be0324a6429ed25b53494a366bab32df7
Sukanta Das <arnobh84@gmail.com>
2023-05-02 09:00:20 +0600

    cart page created

---
+++ pages/cart/index.tsx
@@ -0,0 +1,12 @@
+import Cart from '@/libs/modules/Cart/cart';
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <Cart />
+        </div>
+    );
+};
+
+export default index;

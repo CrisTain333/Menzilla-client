8c99a79f4b5fcf7ff0deb9e97e69ce0f6ce24cbb
Sukanta Das <arnobh84@gmail.com>
2023-05-04 15:10:49 +0600

    track order component created

--- libs/Components/Profile/ProfileContent/ProfileContent.tsx
+++ libs/Components/Profile/ProfileContent/ProfileContent.tsx
@@ -10,6 +10,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import AllOrders from '../AllOrder/AllOrder';
 import AllRefundOrders from '../AllRefundOrders/AllRefundOrders';
+import TrackOrder from '../TrackOrder/TrackOrder';
 
 const ProfileContent = ({ active }: any) => {
     const { currentUser } = useAuth();
@@ -161,99 +162,6 @@ const ProfileContent = ({ active }: any) => {
                     <Address />
                 </div>
             )}
-        </div>
-    );
-};
-
-const TrackOrder = () => {
-    const orders = [
-        {
-            _id: '7463hvbfbhfbrtr28820221',
-            orderItems: [
-                {
-                    name: 'Iphone 14 pro max'
-                }
-            ],
-            totalPrice: 120,
-            orderStatus: 'Processing'
-        }
-    ];
-
-    const columns = [
-        { field: 'id', headerName: 'Order ID', minWidth: 150, flex: 0.7 },
-        {
-            field: 'status',
-            headerName: 'Status',
-            minWidth: 130,
-            flex: 0.7,
-            cellClassName: (params: any) => {
-                return params.getValue(params.id, 'status') === 'Delivered'
-                    ? 'greenColor'
-                    : 'redColor';
-            }
-        },
-        {
-            field: 'itemsQty',
-            headerName: 'Items Qty',
-            type: 'number',
-            minWidth: 130,
-            flex: 0.7
-        },
-
-        {
-            field: 'total',
-            headerName: 'Total',
-            type: 'number',
-            minWidth: 130,
-            flex: 0.8
-        },
-        {
-            field: ' ',
-            flex: 1,
-            minWidth: 130,
-            headerName: '',
-            type: 'number',
-            sortable: false,
-            renderCell: (params: any) => {
-                return (
-                    <>
-                        <Link href={`/order/${params.id}`}>
-                            <button>
-                                <MdOutlineTrackChanges size={20} />
-                            </button>
-                        </Link>
-                    </>
-                );
-            }
-        }
-    ];
-
-    const row: any[] = [];
-
-    orders &&
-        orders.forEach((item) => {
-            row.push({
-                id: item._id,
-                itemsQty: item.orderItems.length,
-                total: 'US$ ' + item.totalPrice,
-                status: item.orderStatus
-            });
-        });
-
-    return (
-        <div className="pl-8 pt-1">
-            {/* <DataGrid
-                rows={row}
-                columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: {
-                            pageSize: 5
-                        }
-                    }
-                }}
-                pageSizeOptions={[5]}
-            /> */}
         </div>
     );
 };

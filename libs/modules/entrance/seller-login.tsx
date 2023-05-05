71528846b78feee7412971d57b3355d7e98fb636
Sukanta Das <arnobh84@gmail.com>
2023-05-05 23:40:27 +0600

    seller login  page created

---
+++ libs/modules/entrance/seller-login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SellerLogin = () => {
+    return (
+        <div>
+            <p>Seller Login</p>
+        </div>
+    );
+};
+
+export default SellerLogin;
---
+++ pages/auth/seller-login/index.tsx
@@ -0,0 +1,12 @@
+import SellerLogin from '@/libs/modules/entrance/seller-login';
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <SellerLogin />
+        </div>
+    );
+};
+
+export default index;

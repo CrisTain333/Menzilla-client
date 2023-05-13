a341fb9a5a96ff004f0534693865eda12d5c0dd9
Sukanta Das <arnobh84@gmail.com>
2023-05-13 09:36:13 +0600

    shop route protected

--- libs/modules/shop/shop.tsx
+++ libs/modules/shop/shop.tsx
@@ -1,6 +1,18 @@
+import { useSeller } from '@/libs/Context/sellerProvider';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const Shop = () => {
+    const { currentSeller } = useSeller();
+    const router = useRouter();
+
+    React.useEffect((): any => {
+        const tokenStoragePath = 'accessToken';
+        const token = localStorage.getItem(tokenStoragePath);
+        if (!token && !currentSeller) {
+            router.push('/');
+        }
+    }, [currentSeller]);
     return <div>Shop</div>;
 };
 

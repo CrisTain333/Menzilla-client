143136f5afefbb9c16b078f9ebddb2e37ff2a4b9
Sukanta Das <arnobh84@gmail.com>
2023-05-13 09:32:39 +0600

    shop routes created

---
+++ pages/shop/index.tsx
@@ -0,0 +1,12 @@
+import Shop from '@/libs/modules/shop/shop';
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <Shop />
+        </div>
+    );
+};
+
+export default index;

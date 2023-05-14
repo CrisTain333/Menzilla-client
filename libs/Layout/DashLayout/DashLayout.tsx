4d0905ca3a6d5cbf9aaec5b5e8a7fe7d6c466b6c
Sukanta Das <arnobh84@gmail.com>
2023-05-14 19:28:30 +0600

    dashboard side bar added

--- libs/Layout/DashLayout/DashLayout.tsx
+++ libs/Layout/DashLayout/DashLayout.tsx
@@ -1,10 +1,14 @@
 import DashboardHeader from '@/libs/Components/Dashboard/DashboardHeader';
+import DashboardSidebar from '@/libs/Components/Dashboard/DashboardSidebar';
 import React from 'react';
 
 const DashLayout = ({ children }: any) => {
     return (
         <div>
             <DashboardHeader />
+            <div className="w-[80px] 800px:w-[330px]">
+                <DashboardSidebar active={1} />
+            </div>
 
             <div>{children}</div>
         </div>

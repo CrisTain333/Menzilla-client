218db59d369e03fec33f9438e147eb804025d49e
Sukanta Das <arnobh84@gmail.com>
2023-05-18 19:47:14 +0600

    create product page added

---
+++ pages/dashboard/create-product.tsx
@@ -0,0 +1,12 @@
+import CreateProduct from '@/libs/modules/dashboard/CreateProduct';
+import React from 'react';
+
+const createProduct = () => {
+    return (
+        <div>
+            <CreateProduct />
+        </div>
+    );
+};
+
+export default createProduct;

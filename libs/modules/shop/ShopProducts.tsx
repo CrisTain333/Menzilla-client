5443adaedf434703f2d480cf85d0287cbea04988
Sukanta Das <arnobh84@gmail.com>
2023-05-20 15:01:52 +0600

    side bar width and all products page added

--- libs/modules/shop/ShopProducts.tsx
+++ libs/modules/shop/ShopProducts.tsx
@@ -1,19 +1,19 @@
 import DashboardHeader from '@/libs/Components/Dashboard/DashboardHeader';
 import DashboardSidebar from '@/libs/Components/Dashboard/DashboardSidebar';
-import CreateProduct from '@/libs/Components/Shop/CreateProduct';
+import ShopPrducts from '@/libs/Components/Shop/ShopPrducts';
 import React from 'react';
 
-const CreateShopProduct = () => {
+const ShopProducts = () => {
     return (
         <div>
             <div>
                 <DashboardHeader />
                 <div className="flex items-center justify-between w-full">
                     <div className="w-20 md:w-72">
-                        <DashboardSidebar active={4} />
+                        <DashboardSidebar active={3} />
                     </div>
                     <div className="w-full justify-center flex">
-                        <CreateProduct />
+                        <ShopPrducts />
                     </div>
                 </div>
             </div>
@@ -21,4 +21,4 @@ const CreateShopProduct = () => {
     );
 };
 
-export default CreateShopProduct;
+export default ShopProducts;

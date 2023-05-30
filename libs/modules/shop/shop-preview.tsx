aa45bffc7e65bb64aaf2a281e17acfec88bba52e
Sukanta Das <arnobh84@gmail.com>
2023-05-30 15:15:57 +0600

    new api added

--- libs/modules/shop/shop-preview.tsx
+++ libs/modules/shop/shop-preview.tsx
@@ -1,23 +1,41 @@
-import { getSellerOnly } from '@/libs/Api';
+import { getSellerOnly, getShopProduct } from '@/libs/Api';
+import { useSeller } from '@/libs/Context/sellerProvider';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
 const ShopPreview = () => {
+    const { allProducts } = useSeller();
     const [sellerInfo, setSellerInfo] = useState<any>();
-    const [currentPage, setCurrentPage] = useState(1);
+    const [products, setProducts] = useState([]);
+
     const router = useRouter();
     const shopId = router?.query?.shopId;
 
     const getSellerDetails = async () => {
         const seller = await getSellerOnly(shopId);
-        setSellerInfo(seller?.seller);
+        if (seller) {
+            setSellerInfo(seller?.seller);
+            const shopProducts = allProducts?.filter(
+                (e: any) => e?.shop?.name === seller?.seller?.name
+            );
+            setProducts(shopProducts);
+        }
     };
 
     useEffect(() => {
         getSellerDetails();
-    }, []);
+    }, [shopId]);
 
-    return <div>Preview :{shopId}</div>;
+    return (
+        <div>
+            Preview: {sellerInfo?.name}
+            <div>
+                {products?.map((e: any, i: any) => {
+                    return <p key={i}>{e.name}</p>;
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default ShopPreview;

be65dfaa8f0879d3ed6ada310c6f28c73c660604
Sukanta Das <arnobh84@gmail.com>
2023-06-20 09:46:46 +0600

    payment page error fixed

--- libs/modules/payment/payment.tsx
+++ libs/modules/payment/payment.tsx
@@ -2,15 +2,19 @@ import CheckoutSteps from '@/libs/Components/Checkout/CheckoutSteps';
 import Payment from '@/libs/Components/Payment/Payment';
 import HeaderAndFooter from '@/libs/Layout/HeaderAndFooter/headerAndFooter';
 import React from 'react';
-
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 const PaymentPage = () => {
+    const stripePromise = loadStripe(process.env.PAYMENT_SECRET_KEY as string);
     return (
         <div>
             <HeaderAndFooter>
                 <br />
                 <br />
                 <CheckoutSteps active={2} />
-                <Payment />
+                <Elements stripe={stripePromise}>
+                    <Payment />
+                </Elements>
                 <br />
                 <br />
             </HeaderAndFooter>

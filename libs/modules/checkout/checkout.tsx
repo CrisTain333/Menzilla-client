a61bc9fe3434db8e5e799c12d9715b28191f8511
Sukanta Das <arnobh84@gmail.com>
2023-06-23 09:21:21 +0600

    update in checkout module

--- libs/modules/checkout/checkout.tsx
+++ libs/modules/checkout/checkout.tsx
@@ -6,13 +6,15 @@ import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 
 const Checkout = () => {
-    const { currentUser } = useAuth();
+    const { currentUser, isLoading } = useAuth();
     const router = useRouter();
     useEffect((): any => {
         const tokenStoragePath = 'accessToken';
         const token = localStorage.getItem(tokenStoragePath);
-        if (!token && !currentUser) {
-            router.push('/');
+        if (!isLoading) {
+            if (!token && !currentUser) {
+                router.push('/auth/login');
+            }
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentUser]);

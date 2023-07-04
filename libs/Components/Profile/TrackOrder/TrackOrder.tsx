39fa3fd5efb71220d7a329979971f6286ce1450d
Sukanta Das <arnobh84@gmail.com>
2023-07-04 15:33:49 +0600

    paginataion added for track ordersection

--- libs/Components/Profile/TrackOrder/TrackOrder.tsx
+++ libs/Components/Profile/TrackOrder/TrackOrder.tsx
@@ -32,7 +32,7 @@ const TrackOrder = () => {
     return (
         <div className="shadow-md w-[90%] mx-auto">
             <div className="pl-5 pr-2 pt-1 pb-5">
-                <div className="overflow-x-auto">
+                <div className="overflow-x-auto scrollbar-hide">
                     <table className="table table-compact w-full">
                         <thead>
                             <tr className="lowercase">
@@ -44,14 +44,14 @@ const TrackOrder = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {orders &&
-                                orders?.map((e: any, i: any) => {
+                            {data &&
+                                data?.map((e: any, i: any) => {
                                     return (
                                         <tr key={i}>
                                             <th>{e?._id}</th>
-                                            <td> {e?.orderStatus}</td>
+                                            <td> {e?.status}</td>
+                                            <td>{e?.cart?.length}</td>
                                             <td>{'US$ ' + e?.totalPrice}</td>
-                                            <td>{e?.orderItems?.length}</td>
                                             <td>
                                                 <Link href={`/order/track-order?orderId=${e?._id}`}>
                                                     <button className="text-blue-500 border p-2">
@@ -65,9 +65,29 @@ const TrackOrder = () => {
                         </tbody>
                     </table>
                 </div>
+
+                <div className="flex justify-start space-x-1 dark:text-gray-100 my-10 ml-10">
+                    {pageNumbers?.map((pageNumber, i) => (
+                        <button
+                            key={i}
+                            onClick={() => handlePageChange(pageNumber)}
+                            type="button"
+                            title="Page 1"
+                            className={`inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md ${
+                                pageNumber === currentPage && 'bg-[#ff9900] text-white'
+                            }`}
+                        >
+                            {pageNumber}
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
     );
 };
 
 export default TrackOrder;
+
+//  <td>
+
+//  </td>;

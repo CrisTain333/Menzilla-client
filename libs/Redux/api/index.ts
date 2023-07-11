477a26d0d3d65274d5436ddcde6e909aead243a5
Sukanta Das <arnobh84@gmail.com>
2023-07-11 10:35:56 +0600

    redux concept pused

--- libs/Redux/api/index.ts
+++ libs/Redux/api/index.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
 
 export const api = createApi({
+    reducerPath: 'api',
     baseQuery: fetchBaseQuery({
         baseUrl: `${process.env.BACKEND_BASE_URL}`
     }),

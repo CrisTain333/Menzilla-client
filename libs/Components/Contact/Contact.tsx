070fd9ede1b860099ec207e56f8c5b2a310e89ab
Sukanta Das <arnobh84@gmail.com>
2023-07-13 15:12:16 +0600

    some change from contact page

--- libs/Components/Contact/Contact.tsx
+++ libs/Components/Contact/Contact.tsx
@@ -123,13 +123,14 @@ const Contact = () => {
                                             height={100}
                                             viewBox="0 0 100 100"
                                             fill="none"
+                                            className="text-green-800"
                                             xmlns="http://www.w3.org/2000/svg"
                                         >
                                             <path
                                                 fillRule="evenodd"
                                                 clipRule="evenodd"
                                                 d="M0 100C0 44.7715 0 0 0 0C55.2285 0 100 44.7715 100 100C100 100 100 100 0 100Z"
-                                                fill="#3056D3"
+                                                fill="#ff9900"
                                             />
                                         </svg>
                                     </span>

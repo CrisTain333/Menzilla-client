1439c86cc6754261ae433a975de5e00c941bf354
Sukanta Das <arnobh84@gmail.com>
2023-11-17 10:19:14 +0600

    error fixed in custom provider

--- libs/providers/custom-provider.tsx
+++ libs/providers/custom-provider.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { store } from '../Redux/store';
 import React from 'react';
 
-const CustomProvider = ({ children }: { children: React.ReactNode }) => {
+const CustomProvider = ({ children }: { children: any }) => {
     return <Provider store={store}>{children}</Provider>;
 };
 
